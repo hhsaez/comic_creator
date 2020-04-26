e1ed74599141e209dc523e60d5f34208015009e9
Hernan Saez <hhsaez@gmail.com>
2020-04-26 17:26:22 -0300

    Prepare pages for printing

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,17 @@
 
 using namespace comic_creator;
 
+using Path = std::filesystem::path;
+using PathArray = std::vector< Path >;
+
 struct PageSize {
 	float width;
 	float height;
+};
+
+struct Settings {
+	PageSize pageSize;
+	PageSize trimSize;
 };
 
 struct Image {
@@ -44,6 +52,23 @@ std::filesystem::path createDirectory( const std::filesystem::path &dir )
 	std::filesystem::create_directories( dir );
 
 	return dir;
+}
+
+std::vector< Path > fetchPages( void )
+{
+	std::vector< Path > ret;
+
+	auto pagesDir = getRootDirectory() / "pages";
+	for ( const auto &entry : std::filesystem::directory_iterator( pagesDir ) ) {
+		if ( std::filesystem::is_directory( entry ) ) {
+			continue;
+		}
+		ret.push_back( entry.path() );
+	}
+
+	std::sort( std::begin( ret ), std::end( ret ) );
+
+	return ret;
 }
 
 std::shared_ptr< Image > loadImage( const std::filesystem::path &path ) noexcept
@@ -93,18 +118,12 @@ void saveImage( std::shared_ptr< Image > const &image, std::filesystem::path con
 	Log::debug( "Saved ", path );
 }
 
-void cutImage( const std::filesystem::path &path, const std::filesystem::path &outDir )
+void cutImage( const std::filesystem::path &path, const std::filesystem::path &outDir, const Settings &settings )
 {
 	auto src = loadImage( path );
 
-	auto pageSize = PageSize {
-		.width = 29.70f,
-		.height = 42.00f
-	};
-	auto trimSize = PageSize{
-		.width = 27.00f,
-		.height = 34.00f
-	};
+	auto pageSize = settings.pageSize;
+	auto trimSize = settings.trimSize;
 
 	auto w0 = int( src->width );
 	auto w1 = int( w0 * trimSize.width / pageSize.width );
@@ -135,38 +154,111 @@ void cutImage( const std::filesystem::path &path, const std::filesystem::path &o
 	saveImage( dst, outDir / path.filename() );
 }
 
-std::filesystem::path createOnlineImages( const std::filesystem::path &src ) noexcept
+Path createOnlineImages( const PathArray &pages, const Settings &settings ) noexcept
 {
 	Log::trace( "Creating images for online publishing" );
 
-	auto outDir = createDirectory( getRootDirectory() / "online" );
+	auto dst = createDirectory( getRootDirectory() / "online" );
+
 	std::vector< std::future< void >> tasks;
-	for ( const auto &entry : std::filesystem::directory_iterator( src ) ) {
-		if ( std::filesystem::is_directory( entry ) ) {
-			continue;
-		}
-		tasks.push_back(
-			std::async(
+	std::transform(
+		std::begin( pages ),
+		std::end( pages ),
+		std::back_inserter( tasks ),
+		[ dst, settings ]( const Path &path ) {
+			return std::async(
 				std::launch::async,
 				cutImage,
-				entry.path(),
-				outDir
-			)
-		);
-	}
+				path,
+				dst,
+				settings
+			);
+		}
+	);
 
-	for ( auto &t : tasks ) {
-		t.get();
-	}
-
-	return outDir;
+	return dst;
 }
 
-std::filesystem::path createPrintingImages( const std::filesystem::path &src )
+void printPages( const Path &left, const Path &right, const Path &dst, const Settings &settings )
 {
+	Log::trace( "Printing ", left.filename(), " and ", right.filename() );
+	
+	auto pageSize = settings.pageSize;
+	auto trimSize = settings.trimSize;
+
+	auto leftImage = loadImage( left );
+	auto rightImage = loadImage( right );
+
+	auto w0 = int( leftImage->width );
+	auto w1 = int( w0 * trimSize.width / pageSize.width );
+	auto h0 = int( leftImage->height );
+	auto h1 = int( h0 * trimSize.height / pageSize.height );
+	auto marginX = int( 0.5 * ( w0 - w1 ) );
+	auto marginY = int( 0.5 * ( h0 - h1 ) );
+
+	auto page = std::make_shared< Image >();
+	page->name = dst.filename();
+	page->width = 2 * leftImage->width;
+	page->height = leftImage->height;
+	page->channels = leftImage->channels;
+	page->data.resize( page->width * page->height * page->channels );
+
+	memset( page->data.data(), 0xff, page->data.size() );
+
+	auto pageStride = page->width * page->channels;
+	auto imageStride = leftImage->width * leftImage->channels;
+
+	for ( auto y = 0l; y < page->height; y++ ) {
+		for ( auto x = 0l; x < leftImage->width; x++ ) {
+			for ( auto c = 0; c < leftImage->channels - 1; c++ ) {
+				auto idx0 = y * pageStride + x * leftImage->channels + c;
+				auto idx1 = y * imageStride + x * leftImage->channels + c;
+				page->data[ idx0 ] = leftImage->data[ idx1 ];
+			}
+		}
+		for ( auto x = 0l; x < rightImage->width; x++ ) {
+			for ( auto c = 0; c < rightImage->channels - 1; c++ ) {
+				auto idx0 = y * pageStride + imageStride + x * page->channels + c;
+				auto idx1 = y * imageStride + x * page->channels + c;
+				page->data[ idx0 ] = rightImage->data[ idx1 ];
+			}
+		}
+	}
+
+	saveImage( page, dst );
+}
+
+Path createPrintingImages( const PathArray &pages, const Settings &settings ) noexcept
+{
+	if ( pages.size() % 4 != 0 ) {
+		Log::error( "Cannot create printing layout. Incorrect number of pages" );
+		return Path();
+	}
+
 	Log::trace( "Creating images for printing" );
 
 	auto dst = createDirectory( getRootDirectory() / "print" );
+	
+	std::vector< std::future< void >> tasks;
+	
+	for ( auto i = 0; i < pages.size() / 2; i++ ) {
+		auto left = pages[ pages.size() - 1 - i ];
+		auto right = pages[ i ];
+
+		std::stringstream ss;
+		ss << "page_" << i << ".png";
+
+		tasks.push_back(
+			std::async(
+				std::launch::async,
+				printPages,
+				left,
+				right,
+				dst / ss.str(),
+				settings
+			)
+		);
+	}
 
 	return dst;
 }
@@ -175,16 +267,26 @@ int main( int argc, char **argv )
 {
 	Log::info( "ComicCreator ", COMIC_CREATOR_VERSION );
 
-	auto currentDir = std::filesystem::current_path();
-	auto pagesDir = currentDir / "pages";
-	if ( !std::filesystem::exists( pagesDir ) ) {
-		Log::error( "Cannot find ", pagesDir );
+	auto settings = Settings {
+		.pageSize = {
+			.width = 29.70f,
+			.height = 42.00f
+		},
+		.trimSize = {
+			.width = 27.00f,
+			.height = 34.00f
+		},
+	};
+
+	auto pages = fetchPages();
+	if ( pages.size() == 0 ) {
+		Log::error( "Cannot fetch pages" );
 		return -1;
 	}
 
-	createOnlineImages( pagesDir );
-	createPrintingImages( pagesDir );
-	
+	createOnlineImages( pages, settings );
+	createPrintingImages( pages, settings );
+
 	return 0;
 }
 
